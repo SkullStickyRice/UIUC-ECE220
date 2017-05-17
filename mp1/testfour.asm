854eec95364931422235ab9197d69467f1d89b8b
SkullStickyRice <lxyestherli@gmail.com>
2017-05-17 16:25:39 -0500

    Add files via upload

---
+++ mp1/testfour.asm
@@ -0,0 +1,6 @@
+	.ORIG x4000
+
+	.STRINGZ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus turpis ac urna scelerisque placerat. Duis suscipit enim scelerisque ex accumsan tincidunt. Aliquam maximus mauris justo, mollis faucibus ex interdum eu. Donec ac elit pharetra, placerat libero eu, pulvinar mauris. In eu sem ipsum. Donec blandit massa at maximus lobortis. Praesent in dignissim augue. Donec at quam dolor. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+
+	.END
+
