c65a8272ee42374e25f989d54c156c7113d470fb
SkullStickyRice <lxyestherli@gmail.com>
2017-05-17 17:02:43 -0500

    Add files via upload

---
+++ mp7/prog9.h
@@ -0,0 +1,28 @@
+struct wav_t{
+  char                RIFF[4];        
+  int                 ChunkSize;     
+  char                WAVE[4];       
+  char                fmt[4];        
+  int                 Subchunk1Size;                              
+  short int           AudioFormat;  
+  short int           NumChan;      
+  int                 SamplesPerSec;  
+  int                 bytesPerSec;    
+  short int           blockAlign;    
+  short int           bitsPerSample;  
+  char               *extra;
+  char                Subchunk2ID[4];
+  int                 Subchunk2Size; 
+  short int          *data;
+};
+
+typedef struct wav_t WAV;
+
+int little_endian_2(FILE *fptr);
+int little_endian_4(FILE *fptr);
+WAV *read_file(char *wavfile);
+void sloop(WAV *inwav, char *outfile, double a, double b, int n);
+int find_copy(WAV *wav1, WAV *wav2);
+// Extra credit
+void lo_pass(WAV *inwav, char *outfile, int freq);
+
